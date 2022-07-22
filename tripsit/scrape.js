3209f2e75b4b61a7ab1c37c7f21ea15cf2d4fd2a
Isaak <isaak.hanimann@gmail.com>
2022-07-22 15:02:14 +0200

    Get substance names of all pages and save them in json file

--- tripsit/scrape.js
+++ tripsit/scrape.js
@@ -1,23 +1,47 @@
 const puppeteer = require('puppeteer')
+const fs = require('fs');
 
 async function scrapeSubstances(url) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
+    let substances = [];
     for (pageNum = 1; pageNum < 20; pageNum++) {
-        await scrapePage(page)
-        console.log("----------------------")
-        await page.$eval( '#DataTables_Table_0_next > a', form => form.click() );
+        const substancesOnPage = await getSubstancesFromPage(page)
+        substances = substances.concat(substancesOnPage)
+        await page.$eval('#DataTables_Table_0_next > a', form => form.click());
     }
+    saveInFile(substances)
     await browser.close()
 }
 
-async function scrapePage(page) {
+async function getSubstancesFromPage(page) {
+    const substancesOnPage = []
     for (i = 1; i < 31; i++) {
-        var element = await page.waitForSelector("#DataTables_Table_0 > tbody > tr:nth-child(" + i + ") > td.ttext.all.sorting_1 > a")
-        var text = await page.evaluate(element => element.textContent, element)
-        console.log(text)
+        try {
+            const element = await page.waitForSelector("#DataTables_Table_0 > tbody > tr:nth-child(" + i + ") > td.ttext.all.sorting_1 > a", {timeout: 5000})
+            const text = await page.evaluate(element => element.textContent, element)
+            substancesOnPage[i - 1] = text
+        } catch (e) {
+            break
+        }
+
     }
+    return substancesOnPage
+}
+
+function saveInFile(substances) {
+    fs.writeFile(
+        "tripsit_substances.json",
+        JSON.stringify(substances, null, 2),
+        'utf8',
+        function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("The data has been scraped and saved successfully! View it at './data.json'");
+        }
+    );
 }
 
 scrapeSubstances('https://drugs.tripsit.me')
